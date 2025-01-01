3a793d25aeab6a9aab797b72685fb8826aea23ec
LuckwurstJoe <93101364+LuckwurstJoe@users.noreply.github.com>
2025-01-01 13:32:05 +0100

    Add files via upload

--- scripts/init.lua
+++ scripts/init.lua
@@ -4,13 +4,12 @@ Tracker:AddItems("items/Seeds.json")
 Tracker:AddItems("items/settings.json")
 
 ScriptHost:LoadScript("scripts/logic.lua")
-ScriptHost:LoadScript("scripts/watches.lua")
 
 Tracker:AddMaps("maps/maps.json")
 Tracker:AddLocations("locations/CommunityCenter.json")
 Tracker:AddLocations("locations/World.json")
 Tracker:AddLocations("locations/Quests.json")
-Tracker:AddLocations("locations/Friendsanity.json")
+--Tracker:AddLocations("locations/Friendsanity.json")
 
 Tracker:AddLayouts("layouts/item_grids.json")
 Tracker:AddLayouts("layouts/tool_grid.json")
@@ -25,6 +24,8 @@ Tracker:AddLayouts("layouts/layouts.json")
 Tracker:AddLayouts("layouts/friends.json")
 Tracker:AddLayouts("layouts/seeds.json")
 Tracker:AddLayouts("layouts/broadcast.json")
+
+ScriptHost:LoadScript("scripts/watches.lua")
 
 if PopVersion and PopVersion >= "0.18.0" then
   ScriptHost:LoadScript("scripts/autotracking.lua")
--- scripts/watches.lua
+++ scripts/watches.lua
@@ -1,7 +1,6 @@
 function updateIslandTracking()
-	AUTOTRACKER_ENABLE_STAT_TRACKING = (Tracker:ProviderCountForCode("island_tracking_on") > 0) and
-		AUTOTRACKER_ENABLE_ITEM_TRACKING
-	if AUTOTRACKER_ENABLE_STAT_TRACKING then
+	AUTOTRACKER_ENABLE_ISLAND_TRACKING = (Tracker:ProviderCountForCode("island_tracking_on") > 0)
+	if AUTOTRACKER_ENABLE_ISLAND_TRACKING then
 		Tracker:AddLayouts("layouts/tracker_island.json")
 	else
 		Tracker:AddLayouts("layouts/tracker.json")
