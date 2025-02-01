eec1c502a4b2b2a7bc039da58b9b3a5fbcb06357
LuckwurstJoe <93101364+LuckwurstJoe@users.noreply.github.com>
2025-02-01 17:11:29 +0100

    Add files via upload

--- scripts/init.lua
+++ scripts/init.lua
@@ -2,18 +2,21 @@ Tracker:AddItems("items/items.json")
 Tracker:AddItems("items/Seeds.json")
 Tracker:AddItems("items/settings.json")
 Tracker:AddItems("items/fishsanity_items.json")
-
+Tracker:AddItems("items/museumsanity_items.json")
+Tracker:AddItems("items/cropsanity_items.json")
+Tracker:AddItems("items/festival_items.json")
 
 ScriptHost:LoadScript("scripts/logic.lua")
 
 Tracker:AddLocations("locations/CommunityCenter.json")
 Tracker:AddLocations("locations/World.json")
 Tracker:AddLocations("locations/Quests.json")
-Tracker:AddLocations("locations/Shipsanity.json")
+Tracker:AddLocations("locations/Cropsanity.json")
 Tracker:AddLocations("locations/Fishsanity.json")
 Tracker:AddLocations("locations/Museum_Donations.json")
 Tracker:AddLocations("locations/Museum_Milestones.json")
 Tracker:AddLocations("locations/Boards.json")
+Tracker:AddLocations("locations/Desert_Festival.json")
 
 Tracker:AddLayouts("layouts/item_grids.json")
 Tracker:AddLayouts("layouts/tool_grid.json")
